e0c4256b2ca8efabf2b5c66fb6ed21828e99e4e7
paul <s8dipaul@stud.uni-saarland.de>
2018-10-24 12:06:57 +0200

    wed 12:00- step 1

--- lock_server.cc
+++ lock_server.cc
@@ -6,9 +6,56 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-lock_server::lock_server():
-  nacquire (0)
+
+pthread_mutex_t mutex_;
+pthread_cond_t lock_free;
+
+vector<lock*> list_locks;
+
+lock::lock(): 
+  STATE(FREE), lock_waiting(0)
 {
+}
+
+lock* get_lock(lockid_t lid)
+{
+  for(vector<lock *>::iterator lock_it = list_locks.begin();
+        lock_it != list_locks.end(); ++lock_it)
+  {
+    lock *l = *lock_it;
+    if(lid == l->lock_id) 
+      return l;
+  }
+  return nullptr; 
+}
+
+lock_server::lock_server():
+  nacquire (0) 
+{
+
+  pthread_mutex_init(&mutex_, 0);
+  pthread_cond_init(&lock_free, 0);
+
+}
+
+lock_protocol::status lock::acquire(lock_protocol::lockid_t lid)
+{
+ assert(pthread_mutex_lock(&mutex_) == 0);
+   lock *l;
+   l = get_lock(lid);
+   if(l){
+    while(1){
+      if(l->get_state() == FREE){
+        l->set_state_locked(); 
+        assert(pthread_mutex_unlock(&mutex_) == 0);
+        return lock_protocol::OK;
+      } else{
+        assert(pthread_mutex_unlock(&mutex_) == 0);
+
+
+    }   
+   }
+  
 }
 
 lock_protocol::status
--- lock_server.h
+++ lock_server.h
@@ -1,13 +1,37 @@
 // this is the lock server
 // the lock client has a similar interface
 
+enum {
+  FREE, LOCKED
+};
+
 #ifndef lock_server_h
 #define lock_server_h
+
 
 #include <string>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+
+class lock{
+  lockid_t lock_id;
+  int STATE;
+  int lock_waiting;
+
+  public:
+    int get_lock_id(return lock_id;)
+    int get_state(return state;)
+    int get_lock_waiting(return lock_waiting;)
+
+    void set_state_locked(){STATE = LOCKED;}
+    void set_state_free(){STATE = FREE;}
+    int inc_lock_waiting(return ++lock_waiting;)
+    int dec_lock_waiting(return --lock_waiting;)
+  
+    lock_protocol::status acquire(lock_protocol::lockid_t);
+    lock_protocol::status release(lock_protocol::lockid_t);
+};
 
 class lock_server {
 
