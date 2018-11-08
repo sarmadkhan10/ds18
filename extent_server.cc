7baec2957bf5c5b314478372b9e5c102dc3885a6
sarmadkhan10 <sarmadkhan10@gmail.com>
2018-11-08 19:48:41 +0100

    Implemented extent server. Times are not being calculated right now.

--- extent_server.cc
+++ extent_server.cc
@@ -8,33 +8,79 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// map for storing file/dir content
+static std::map<extent_protocol::extentid_t, std::string> file_storage;
+static std::map<extent_protocol::extentid_t, extent_protocol::attr> file_attr;
+
 extent_server::extent_server() {}
 
 
 int extent_server::put(extent_protocol::extentid_t id, std::string buf, int &)
 {
-  return extent_protocol::IOERR;
+  // store the file/dir content
+  file_storage[id] = buf;
+
+  // update attr i.e. times
+  extent_protocol::attr new_attr;
+  new_attr.size = 0;
+  new_attr.atime = 0; //TODO: calculate these times
+  new_attr.mtime = 0;
+  new_attr.ctime = 0;
+
+  file_attr[id] = new_attr;
+
+  return extent_protocol::OK;
 }
 
 int extent_server::get(extent_protocol::extentid_t id, std::string &buf)
 {
-  return extent_protocol::IOERR;
+  buf = file_storage[id];
+
+  return extent_protocol::OK;
 }
 
 int extent_server::getattr(extent_protocol::extentid_t id, extent_protocol::attr &a)
 {
-  // You replace this with a real implementation. We send a phony response
-  // for now because it's difficult to get FUSE to do anything (including
-  // unmount) if getattr fails.
-  a.size = 0;
-  a.atime = 0;
-  a.mtime = 0;
-  a.ctime = 0;
-  return extent_protocol::OK;
+  int ret_val = extent_protocol::NOENT;
+
+  std::map<extent_protocol::extentid_t, extent_protocol::attr>::iterator it;
+  
+  it = file_attr.find(id);
+
+  if(it != file_attr.end()) {
+    a.size = it->second.size;
+    a.atime = it->second.atime;
+    a.mtime = it->second.mtime;
+    a.ctime = it->second.ctime;
+
+    ret_val = extent_protocol::OK;
+  }
+
+  return ret_val;
 }
 
 int extent_server::remove(extent_protocol::extentid_t id, int &)
 {
-  return extent_protocol::IOERR;
+  int ret_val = extent_protocol::NOENT;
+
+  std::map<extent_protocol::extentid_t, std::string>::iterator it_file;
+  std::map<extent_protocol::extentid_t, extent_protocol::attr>::iterator it_attr;
+
+  it_file = file_storage.find(id);
+
+  if(it_file != file_storage.end()) {
+    file_storage.erase(it_file);
+
+    it_attr = file_attr.find(id);
+
+    assert(it_attr != file_attr.end());
+    
+    file_attr.erase(it_attr);
+
+    ret_val = extent_protocol::OK;
+
+  }
+
+  return ret_val;
 }
 
